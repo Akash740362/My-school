9ff7443d9b957d4d4852e3bbc4998b13cabafce7
Akash740362 <gauakash01@gmail.com>
2025-01-06 11:49:59 +0530

    Add new code

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 import java.util.Scanner;
 public class Abc{
     public static void main(String[] args){
-    <br>
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter a number");
         int a = sc.nextInt();
--- Test.java
+++ Test.java
@@ -1,7 +1,13 @@
 package My-school;
-
+import java .util.Scanner;
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello world";)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a number:");
+        int a = sc.nextInt();
+        System.out.print("Please enter a number:");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum is: "+ sum);
     }
 }
